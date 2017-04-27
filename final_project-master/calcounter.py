7543f2940f09953a5538f09ab4f490933ec40745
syan456 <syan456@brandeis.edu>
2017-04-27 12:42:13 -0400

    calcounter 2 .py

--- final_project-master/calcounter.py
+++ final_project-master/calcounter.py
@@ -1,6 +1,18 @@
-def calorie_intake(gender, weight, height, age):
+def calorie_intake():
     #calculates the number calories one should intake
     #using Harris Benedict BMR equation
+
+    print("This program will calculate number of calories you should consume per day.")
+    print("Please note this calculator may not be accurate for infants and young children.")
+
+
+    #while gender != "male" or gender != "female":
+        #gender= input("Error, please type in gender again")
+
+    weight= float(input("How much do you weigh in pounds?>> "))
+    height= float(input("How tall are you in inches?>> "))
+    age= float(input("How many years old are you?>> "))
+    gender= input("Are you male or female?>> ")
     answer = 0
     if gender== "male":
         x_male= 66+6.2*(weight)+12.7*(height)-6.76*(age)
@@ -9,13 +21,18 @@ def calorie_intake(gender, weight, height, age):
     elif gender== "female":
         x_female= 655.1+4.35*(weight)+4.7*(height)-4.7*(age)
         answer = x_female
-        #x_female is the number of calories a female should consume
-    return int(answer)
+    else:
+        while gender != "male" or gender != "female":
+            gender= input("Error, please type in gender again")
+
+#x_female is the number of calories a female should consume
+    #return int(answer)
     #need to fix error checking for male, in the case that gender is inputted incorrectly
 
+    print ("You should consume " + str(answer) + " calories daily")
 
 
-if __name__=="__main__":
+'''if __name__=="__main__":
     print("This program will calculate number of calories you should consume per day.")
     print("Please note this calculator may not be accurate for infants and young children.")
 
@@ -30,4 +47,4 @@ if __name__=="__main__":
 
     final_answer = calorie_intake(gender, weight, height, age)
 
-    print ("You should consume " + str(final_answer) + " calories daily")
+    print ("You should consume " + str(final_answer) + " calories daily")'''
