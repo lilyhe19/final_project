44fbf620bddb18dd1db6b279813e03b7934cf702
syan456 <syan456@brandeis.edu>
2017-04-28 21:45:18 -0400

    sleepcal 2-- using list of dictionaries

--- final_project_master/sleepcal.py
+++ final_project_master/sleepcal.py
@@ -1,16 +1,31 @@
-def sleep(number):
-    #tells you how much sleep you should get based on age
-    #infants not included
+def sleep():
+    '''
+    This function puts out the recommended number of hours of sleep per night according to the guidelines
+    set by The National Sleep Foundation. The output is formatted as a range of hours, as there is no
+    perfect formula for determining how much sleep one should get, and these numbers are simply recommendations
+    based on the newest sleep research.
+
+    You can type in your/your child's age, and the function will give you a recommendation based on your input.
+    This function does not give reliable recommendations for children under 1 year old.
+    '''
+    print("\n This program returns the recommended number of hours of sleep per night \
+    \n according to the age-based guidelines set by The National Sleep Foundation. \
+    \n Note: this calculator may not be accurate for infants and very young children (under the age of 1).")
 
     master= [{'age':[1,2],'hours':"11-14"},{'age':[3,4,5],'hours':"10-13"},{'age':[6,7,8,9,10,11,12,13],'hours':"9-11"},{'age':[14,15,16,17],'hours':"8-10"}]
+
+    number = int(input("Please enter an age (between 1-17 years): "))
+
     for x in master:
         if number in x['age']:
-            return(x['hours'])  
+            print("Your child should sleep " + str(x['hours']) + " hours." )
 
-if __name__ == "__main__":
 
-    print("This program will tell you how many hours your child should sleep based on his/her age.")
-    print("This program does apply to infants (children under one year old)")
-    number = int(input("Please enter in an age (1-17 years): "))
-    final_answer = sleep(number)
-    print("Your child should sleep " + str(final_answer) + " hours.")
+#if __name__ == "__main__":
+
+    #print("\n This program returns the recommended number of hours of sleep per night \
+    #\n according to the age-based guidelines set by The National Sleep Foundation. \
+	#\n Note: this calculator may not be accurate for infants and very young children (under the age of 1).")
+    #number = int(input("Please enter in an age (between 1-17 years): "))
+    #final_answer = sleep(number)
+    #print("Your child should sleep " + str(final_answer) + " hours.")
