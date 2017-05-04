573883e084007166204de7629425da73225df93a
lilyhe19 <lily.he19@gmail.com>
2017-05-04 04:05:40 -0400

    added specialty filter, haven't tested it yet

--- doc_search.py
+++ doc_search.py
@@ -1,6 +1,7 @@
 def docs():
     '''
-    Takes user input of location and returns the three doctors from nearest location (matching town/city).
+    Takes user input of location and returns the three doctors
+    from nearest location (matching town/city).
     '''
 
     print("\n This program will find three doctors \
@@ -11,26 +12,32 @@ def docs():
     import csv
     csv_file = csv.DictReader(open('DoctorSearch.csv'))
     input_city = input("Please type in the name of your city/town. (first letter uppercase, remaining letters in lowercase) \n ->  ")
+    input_specialty = input("Please type in the name of the medical specialty you are looking for if this is applicable. \n If not, press Enter to continue. (first letter uppercase, remaining letters in lowercase; ie. Radiology, Oncology \n -> ")
 
-    #TENTATIVE CODE 
+    #TENTATIVE CODE
     # found = False #use boolean to error check
-    
-    # while found == False: 
-    #     for x in row['City']: 
-    #         if input_city != x: 
+
+    # while found == False:
+    #     for x in row['City']:
+    #         if input_city != x:
     #             found = True
     #     input_city = input("Please try another city/town. \n ->")
 
-
     closest = [row for row in csv_file if row['City'] == input_city]
+
+    if input_specialty != []:
+        closest = [row for row in csv_file if row['City'] == input_city and row['Specialty' == input_specialty]
+    elif input_specialty == []:
+        closest = [row for row in csv_file if row['City'] == input_city]
 
     first = closest[0]
     second = closest[1]
     third = closest[2]
 
-    return print("The three doctors nearest you are: \n 1.", first['\ufeffFirst Name'], first['Last Name'], "at", first['Hospital'], "\
-    \n 2.",second['\ufeffFirst Name'],second['Last Name'],"at",second['Hospital'], "\
-    \n 3.",third['\ufeffFirst Name'],third['Last Name'],"at",third['Hospital'])
+    return print("The three doctors nearest you are: \n 1.", first['\ufeffFirst Name'], first['Last Name'], "at", first['Hospital'],"who specializes in",second['Specialty'], "\
+    \n 2.",second['\ufeffFirst Name'],second['Last Name'],"at",second['Hospital'],"who specializes in",second['Specialty'], "\
+    \n 3.",third['\ufeffFirst Name'],third['Last Name'],"at",third['Hospital'],"who specializes in",second['Specialty'])
+
 
 
 #if __name__ == "__main__":
