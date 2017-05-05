a346e210c7e847af7cb9af9ef6bd269f68803595
lilyhe19 <lily.he19@gmail.com>
2017-05-05 00:50:03 -0400

    removed comments

--- FINAL/caltesting.py
+++ FINAL/caltesting.py
@@ -8,15 +8,6 @@ def calorie_intake():
 	print("This program will calculate number of calories you should consume per day.")
 	print("Please note this calculator may not be accurate for infants and young children.")
 
-	#while gender != "male" or gender != "female":
-	#gender= input("Error, please type in gender again")
-
-	# weight= float(input("How much do you weigh in pounds?>> "))
-	# height= float(input("How tall are you in inches?>> "))
-	# age= float(input("How many years old are you?>> "))
-	# gender = input("Please type in gender: ")
-	# answer = 0
-
 	gender = input("Please type in your birth gender (male/female):  ")
 
 	while gender != "male" and gender != "female":
@@ -42,25 +33,6 @@ def calorie_intake():
 		x_female = 655.1+4.35*(weight)+4.7*(height)-4.7*(age)
 		answer = x_female
 
-	# while True: #use "is not" to compare strings
-	# #print(gender)
-	# 	gender = input("Error, please type in gender again")
-	# 	if gender != "male" or "female":
-	# 		print(gender)
-	# 		print(gender=="male")
-	# 		gender=''
-	# 	elif gender== "male":
-	# 		x_male= 66+6.2*(weight)+12.7*(height)-6.76*(age)
-	# 		answer = x_male
-	# 		#x_male is number of calories a male should consume
-	# 	elif gender== "female":
-	# 		x_female= 655.1+4.35*(weight)+4.7*(height)-4.7*(age)
-	# 		answer = x_female
-
-#x_female is the number of calories a female should consume
-#return int(answer)
-#need to fix error checking for male, in the case that gender is inputted incorrectly
-
 	return print ("You should consume " + str(answer) + " calories daily.")
 
 
@@ -69,10 +41,6 @@ print("This program will calculate the number of calories you should consume per
 print("Please note this calculator may not be accurate for infants and young children.")
 
 gender= input("Are you male or female?>> ")
-
-#while gender != "male" or gender != "female":
-#gender= input("Error, please type in gender again")
-#else:
 weight= float(input("How much do you weigh in pounds?>> "))
 height= float(input("How tall are you in inches?>> "))
 age= float(input("How many years old are you?>> "))
