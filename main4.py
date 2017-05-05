61b976e9c50a44f7b7d5cae4a29d1fb8d126e733
syan456 <syan456@brandeis.edu>
2017-05-05 00:24:40 -0400

    Update main4.py

--- main4.py
+++ main4.py
@@ -29,85 +29,85 @@ def main():
 
     while mainMenuInput != '0':
         if mainMenuInput.isdigit() == False: #error check if input is out of range
-            mainMenuInput = input("Please type a number from 1 to 5: ")
+            mainMenuInput = input("Please type a number from 1 to 5:  ")
 
-        elif int(mainMenuInput) not in [1,2,3,4,5,6,7]:
+        elif int(mainMenuInput) not in [1,2,3,4,5]:
             mainMenuInput = input("Please type a number from 1 to 5: ")
 
         elif int(mainMenuInput) == 1: #call each separate method function
             import doc_search #imports function
             doc_search.docs()
-            repeat1 = input("Would you like to use this function again? (y/n)  ")
+            repeat1 = input("Would you like to use this function again? (y/n):  ")
             while True:
                 if repeat1 == 'y':
                     import doc_search
                     doc_search.docs()
-                    repeat1 = input("Would you like to use this function again? (y/n)  ")
+                    repeat1 = input("Would you like to use this function again? (y/n):  ")
                 elif repeat1 == 'n':
                     break
                 else:
-                    repeat1 = input("Please enter either \"y\" or \"n\".")
+                    repeat1 = input("Please enter either \"y\" or \"n\":  ")
             break
 
         elif int(mainMenuInput) == 2:
             import bmi #imports function
             bmi.BMI()
-            repeat2 = input("Would you like to use this function again? (y/n)  ")
+            repeat2 = input("Would you like to use this function again? (y/n):  ")
             while True:
                 if repeat2 == 'y':
                     import bmi
                     bmi.BMI()
-                    repeat2 = input("Would you like to use this function again? (y/n)  ")
+                    repeat2 = input("Would you like to use this function again? (y/n):  ")
                 elif repeat2 == 'n':
                     break
                 else:
-                    repeat2 = input("Please enter either \"y\" or \"n\".")
+                    repeat2 = input("Please enter either \"y\" or \"n\":  ")
             break
 
         elif int(mainMenuInput) == 3:
             import caltesting
             caltesting.calorie_intake()
-            repeat3 = input("Would you like to use this function again? (y/n)  ")
+            repeat3 = input("Would you like to use this function again? (y/n):  ")
             while True:
                 if repeat3 == 'y':
                     import caltesting
                     caltesting.calorie_intake()
-                    repeat3 = input("Would you like to use this function again? (y/n)  ")
+                    repeat3 = input("Would you like to use this function again? (y/n):  ")
                 elif repeat3 == 'n':
                     break
                 else:
-                    repeat3 = input("Please enter either \"y\" or \"n\".")
+                    repeat3 = input("Please enter either \"y\" or \"n\":  ")
             break
 
         elif int(mainMenuInput) == 4:
             import essentials
             essentials.checklist()
-            repeat4 = input("Would you like to use this function again? (y/n)  ")
+            repeat4 = input("Would you like to use this function again? (y/n):  ")
             while True:
                 if repeat4 == 'y':
                     import essentials
                     essentials.checklist()
-                    repeat4 = input("Would you like to use this function again? (y/n)  ")
+                    repeat4 = input("Would you like to use this function again? (y/n):  ")
                 elif repeat4 == 'n':
                     break
                 else:
-                    repeat4 = input("Please enter either \"y\" or \"n\".")
+                    repeat4 = input("Please enter either \"y\" or \"n\":  ")
             break
 
         elif int(mainMenuInput) == 5:
             print('aloha')
             import sleepcal_alt
             sleepcal_alt.sleep()
-            repeat5 = input("Would you like to use this function again? (y/n)  ")
+            repeat5 = input("Would you like to use this function again? (y/n):  ")
             while True:
                 if repeat5 == 'y':
                     import sleepcal_alt
                     sleepcal_alt.sleep()
-                    repeat5 = input("Would you like to use this function again? (y/n)  ")
+                    repeat5 = input("Would you like to use this function again? (y/n):  ")
                 elif repeat5 == 'n':
                     break
                 else:
-                    repeat5 = input("Please enter either \"y\" or \"n\".")
+                    repeat5 = input("Please enter either \"y\" or \"n\":  ")
             break
 
         else:
@@ -117,15 +117,15 @@ def main():
 
 if __name__ == "__main__":
     main()
-    mainMenuReturn = input("\n Would you like to return to the main menu? (y/n)  ")
+    mainMenuReturn = input("\n Would you like to return to the main menu? (y/n):  ")
     while True:
         if mainMenuReturn == 'y':
             main()
             mainMenuReturn = ""
-            mainMenuReturn= input("\n Would you like to return to the main menu? (y/n)  ")
+            mainMenuReturn= input("\n Would you like to return to the main menu? (y/n):  ")
         elif mainMenuReturn == 'n':
             print("Thanks for using EZHealth today!")
             break
         else: #if types in wrong
-             mainMenuReturn = input("Please enter either \"y\" or \"n\".")
+             mainMenuReturn = input("Please enter either \"y\" or \"n\":  ")
          #problem here where, on second time, it skips to this line bc of the else statement
